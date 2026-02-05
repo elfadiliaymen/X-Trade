faeab47ec9e750a4a364cc791ee574cc1b4f977f
aymen ELFADILI <aymenelfadili0@gmail.com>
2026-02-05 17:06:02 +0100

    Xtrade partie 2

--- src/main/java/org/example/TradingPlatform.java
+++ src/main/java/org/example/TradingPlatform.java
@@ -1,9 +1,11 @@
 package org.example;
 
+
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 public class TradingPlatform {
     private String name;
@@ -110,7 +112,7 @@ public void wantsTobuy( String traderid , String assetcode , int quantity ){
 
         trader.buyAsset(asset , quantity);
         trader.setBalance(trader.getBalance() - asset.getPrice() * quantity);
-        transactions.add(new Transaction("BUY" , assetcode, quantity , asset.getPrice() , LocalDateTime.now()));
+        transactions.add(new Transaction(traderid , "BUY" , assetcode, quantity , asset.getPrice() , LocalDateTime.now()));
 
 };
 
@@ -137,7 +139,7 @@ public void wantsTobuy( String traderid , String assetcode , int quantity ){
 
             trader.sellAsset(asset , quantity);
             trader.setBalance(trader.getBalance() + asset.getPrice() * quantity);
-        transactions.add(new Transaction("SELL"  ,assetcode, quantity , asset.getPrice() , LocalDateTime.now()));
+        transactions.add(new Transaction(traderid , "SELL"  ,assetcode, quantity , asset.getPrice() , LocalDateTime.now()));
 
 
     };
@@ -273,6 +275,208 @@ public void wantsTobuy( String traderid , String assetcode , int quantity ){
         System.out.println("Montant investi : " + totalInvested + "$");
         System.out.println("Valeur actuelle du portefeuille : " + currentValue + "$");
         System.out.println("Gain / Perte : " + gainOrLoss + "$");
+    }
+
+//< --------------- partie 2 : --------------------->
+
+    public void getTransactionOfTrader(String traderID){
+        Trader traderTofind = findTrader(traderID);
+        if (traderTofind == null) {
+            System.out.println("Trader non trouvé");
+            return;
+        }
+
+      List<Transaction> traderTransactions  = transactions.stream()
+              .filter(t -> t.getTraderId().equals(traderID))
+              .toList();
+
+        if (traderTransactions.isEmpty()) {
+            System.out.println("Aucune transaction trouvée");
+            return;
+        }
+        System.out.println("---- Transactions du trader " + traderTofind.getName() + " ----");
+        traderTransactions.forEach(System.out::println);
+
+    }
+
+
+
+    public void filterTransactions(
+            String traderId,
+            String type,
+            String assetCode,
+            LocalDateTime startDate,
+            LocalDateTime endDate
+    ) {
+
+        List<Transaction> result = transactions.stream()
+                .filter(t ->  t.getTraderId().equals(traderId))
+                .filter(t ->  t.getType().equalsIgnoreCase(type))
+                .filter(t ->  t.getAssetCode().equalsIgnoreCase(assetCode))
+                .filter(t -> !t.getDate().isBefore(startDate))
+                .filter(t -> !t.getDate().isAfter(endDate))
+                .toList();
+
+        if (result.isEmpty()) {
+            System.out.println("Aucune transaction trouvée avec ces critères");
+            return;
+        }
+
+        System.out.println("---- Transactions filtrées ----");
+        result.forEach(System.out::println);
+    }
+
+    public void sortBydate(){
+        List<Transaction> sortedtransactionList = transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getDate))
+                .toList();
+
+        if ( sortedtransactionList.isEmpty()){
+            System.out.println("Aucune transaction à trier");
+            return;
+        }
+
+        System.out.println("---- Transactions triées par date ----");
+        sortedtransactionList.forEach(System.out::println);
+    }
+
+    public void sortByMontant(){
+        List<Transaction> sortedList = transactions.stream()
+                .sorted(Comparator.comparing(t -> t.getPrice() * t.getQuantity())).toList();
+
+        if( sortedList.isEmpty()){
+            System.out.println("Aucune transaction à trier");
+            return;
+        }
+        System.out.println("---- Transactions triées par Montant ----");
+        sortedList.forEach(System.out::println);
+    }
+
+public void getTotalVolumeTraded(){
+      Map< String , Integer> volume = transactions.stream()
+              .collect(groupingBy(Transaction::getAssetCode , summingInt(Transaction::getQuantity)));
+
+      volume.forEach((asset , qty) ->
+              System.out.println("Asset: " + asset + " | Volume total: " + qty));
+
+
+}
+
+public void getTotalAmountTradedByBuy(){
+        Map <String , Double > totalBuyAmount = transactions.stream()
+                .filter(t -> t.getType().equalsIgnoreCase("BUY"))
+                .collect(
+                        groupingBy(
+                                t -> t.getAssetCode(), summingDouble(t -> t.getPrice() * t.getQuantity())
+                        )
+                );
+    totalBuyAmount.forEach((asset, amount) ->
+            System.out.println("Asset: " + asset + " | Montant total des achats: " + amount)
+    );
+}
+
+
+    public void getTotalAmountTradedBySell(){
+        Map<String , Double> totalSellAmount = transactions.stream()
+                .filter(t -> t.getType().equalsIgnoreCase("SELL"))
+                .collect(
+                        groupingBy(
+                                t-> t.getAssetCode() , summingDouble(t -> t.getPrice() * t.getQuantity())
+                        )
+                );
+
+        totalSellAmount.forEach((asset, amount) ->
+                System.out.println("Asset: " + asset + " | Montant total des ventes: " + amount)
+        );
+    }
+
+
+    public void getTotalVolumeByTrader(){
+        Map<String , Integer> totalVolumeByTrader = transactions.stream()
+                .collect(
+                        groupingBy(Transaction::getTraderId , summingInt(Transaction::getQuantity))
+                );
+        totalVolumeByTrader.forEach((trader, volume) ->
+                System.out.println("Trader ID: " + trader + " | Volume total échangé: " + volume)
+        );
+    }
+
+    public void getTotalOrders() {
+        long totalOrders = transactions.stream().count();
+        System.out.println("Nombre total d’ordres passés : " + totalOrders);
+    }
+
+    public void getTopTradersByVolume(int topN){
+        Map<String , Integer> volumeByTrader = transactions.stream()
+                .collect(
+                        groupingBy(Transaction::getTraderId ,
+                                summingInt(Transaction::getQuantity)
+
+                        ));
+
+        List< Map.Entry<String , Integer>> topTraders = volumeByTrader.entrySet().stream()
+                .sorted((e1 , e2) -> e2.getValue().compareTo(e1.getValue()))
+                .limit(topN)
+                .toList();
+
+        System.out.println("----- Top " + topN + " Traders par volume -----");
+        topTraders.forEach(e ->
+                System.out.println("Trader ID: " + e.getKey() + " | Volume total: " + e.getValue())
+        );
+    }
+
+
+
+    public void getTotalVolumeByInstrument(){
+        Map<String , Integer> volumeByInstrument = transactions.stream()
+                .collect(
+                        groupingBy(
+                                t -> t.getAssetCode() ,
+                                summingInt( t -> t.getQuantity())
+                        )
+                );
+
+        System.out.println("---- Volume total échangé par instrument ----");
+        volumeByInstrument.forEach((assetCode , volume) ->
+                        System.out.println("Instrument: " + assetCode  + " | Volume total: " + volume)
+                );
+    }
+
+    public void getMostTradedInstrument(){
+        Map<String , Integer>  volumeByInstrument = transactions.stream()
+                .collect(
+                        groupingBy(
+                                t -> t.getAssetCode() ,
+                                summingInt(t -> t.getQuantity())
+                        )
+                );
+        volumeByInstrument.entrySet().stream()
+                .sorted((e1 , e2) -> e2.getValue().compareTo(e1.getValue()))
+                .limit(1)
+                .forEach(e ->
+                                System.out.println(
+                                        "Instrument le plus échangé : "
+                                                + e.getKey()
+                                                + " | Volume total : "
+                                                + e.getValue()
+                                ));
+    }
+
+
+
+    public void getTotalBuyAndSellAmounts(){
+        double totalBuy = transactions.stream()
+                .filter(t -> t.getType().equalsIgnoreCase("BUY"))
+                .mapToDouble(t -> t.getPrice() * t.getQuantity())
+                .sum();
+
+        double totalSell = transactions.stream()
+                .filter(t -> t.getType().equalsIgnoreCase("SELL"))
+                .mapToDouble(t -> t.getPrice() * t.getQuantity())
+                .sum();
+
+        System.out.println("Montant total des BUY  : " + totalBuy);
+        System.out.println("Montant total des SELL : " + totalSell);
 
     }
 }
